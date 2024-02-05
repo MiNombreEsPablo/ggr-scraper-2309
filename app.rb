71520500f86d2da2faf34a701a57081d02b416df
Pablo Bugueno <pablo.buguenoe@gmail.com>
2024-02-05 10:53:13 +0900

    added auto_formatter bifurcation

--- app.rb
+++ app.rb
@@ -15,5 +15,18 @@ puts 'Reviewing possibly non scraped articles...'
 repo.update if repo.non_parsed.any?
 puts 'Scraping process finished!'
 puts "#{repo.all.size} articles scraped."
-# todo: auto formatter when relevant
+
+if repo.all.size < 17_000 && to.zero?
+  puts 'Running formatter...'
+  require_relative 'lib/formatter'
+  input_csv = CSV.open('./results/articles.csv', 'r', encoding: 'UTF-8')
+  output_csv = CSV.open('./results/formatted.csv', 'w', encoding: 'UTF-8')
+
+  process_rows(input_csv, output_csv)
+
+  input_csv.close
+  output_csv.close
+end
+
+`play -q lib/bleep.wav`
 puts 'Done!'
